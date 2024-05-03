f82f2cf99c452ea68378fcafbd0c5a1dc9c2444e
No0ne003 <hi.yassir.op@gmail.com>
2024-05-03 18:22:42 +0100

    change the name from React-Project to React-Projects

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Project/",
+  base: "/React-Projects/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
