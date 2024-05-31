1afe0fdc76743a9a2aeb24e6b7a0700133a23167
No0ne003 <hi.yassir.op@gmail.com>
2024-05-31 11:04:39 +0100

    refactore: remove WhatTheme hook

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,25 +1,6 @@
-import { useTheme } from "@/components/theme-provider";
 import { clsx } from "clsx";
-import { useEffect } from "react";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
-}
-
-// know what theme is set and set theme
-export default function useWhatTheme() {
-  const { theme, setTheme } = useTheme();
-
-  useEffect(() => {
-    if (theme === "system") {
-      setTheme(
-        window.matchMedia("(prefers-color-scheme: dark)")?.matches
-          ? "dark"
-          : "light",
-      );
-    }
-  });
-
-  return { theme };
 }
